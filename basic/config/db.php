f90c2625a18ceed08ca8cd367ca412cc826e7634
alexshynk <shurik.shynk@gmail.com>
2016-11-12 10:23:08 +0200

    commit 2

--- basic/config/db.php
+++ basic/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2_g',
-    'username' => 'alex',
-    'password' => 'alex',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
 ];
